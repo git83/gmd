d30cff70f3e11b454edaca581f45e1205fadcb36
rnedkov <radi.nedkov@gmail.com>
2015-09-11 17:59:58 +0200

    gg12

--- core/src/main/java/com/github/git83/gmd/Main.java
+++ core/src/main/java/com/github/git83/gmd/Main.java
@@ -3,6 +3,6 @@ package com.github.git83.gmd;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test gg");
-		System.out.println("GG");
+		System.out.println("GG12");
 	}
 }
