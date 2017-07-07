af7a8988fdeb6ad493b8056985abe62794c3a15b
nedkov <radi.nedkov@gmail.com>
2017-07-07 22:27:13 +0200

    test 4321

--- core/src/main/java/com/github/git83/gmd/Main.java
+++ core/src/main/java/com/github/git83/gmd/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test gg");
 		System.out.println("1234");
+		System.out.println("4321");
 		System.out.println("aaaa");
 	}
 }
