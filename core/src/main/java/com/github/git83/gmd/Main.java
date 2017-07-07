27bbbea79b4a2752638b93cf1fe9853ff6333cb9
nedkov <nedkov@nedkov-ThinkPad-T500>
2017-07-07 22:00:55 +0200

    test

--- core/src/main/java/com/github/git83/gmd/Main.java
+++ core/src/main/java/com/github/git83/gmd/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test gg");
 		System.out.println("1234");
-		System.out.println("abcd");
 		System.out.println("aaaa");
 	}
 }
