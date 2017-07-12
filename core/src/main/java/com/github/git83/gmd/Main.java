dd2915bc8680c1cbf5a9307de392423d401f2172
nedkov <radi.nedkov@gmail.com>
2017-07-12 23:29:35 +0200

    test bbbb

--- core/src/main/java/com/github/git83/gmd/Main.java
+++ core/src/main/java/com/github/git83/gmd/Main.java
@@ -5,6 +5,6 @@ public class Main {
 		System.out.println("Test gg");
 		System.out.println("1234");
 		System.out.println("4321");
-		System.out.println("aaaa");
+		System.out.println("bbbb");
 	}
 }
